485ff6f0809f29c6309199e4d95f560056e4f9a2
Umar Kulukeev <uumar686@gmail.com>
2022-11-30 17:34:42 +0600

    saving training data

--- lib/shared/menu_drawer.dart
+++ lib/shared/menu_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/screens/sessions_screen.dart';
 import 'package:fitness/screens/weather_screen.dart';
 import 'package:flutter/material.dart';
 import '../screens/bmi_screen.dart';
@@ -43,6 +44,9 @@ class MenuDrawer extends StatelessWidget {
               break;
             case 'Weather':
               screen = WeatherScreen();
+              break;
+            case 'Training':
+              screen = SessionScreen();
               break;
           }
           Navigator.of(context).pop();
